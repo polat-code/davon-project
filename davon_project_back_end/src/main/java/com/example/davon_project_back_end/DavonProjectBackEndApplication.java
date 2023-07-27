b37789a94b2418f5e39810d28f546ae126e54411
ozgurhanpolat <ozgurhan.45@gmail.com>
2023-07-27 10:45:32 +0300

    Backend is added

---
+++ davon_project_back_end/src/main/java/com/example/davon_project_back_end/DavonProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.davon_project_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavonProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavonProjectBackEndApplication.class, args);
+	}
+
+}
---
+++ davon_project_back_end/src/test/java/com/example/davon_project_back_end/DavonProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.davon_project_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DavonProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
