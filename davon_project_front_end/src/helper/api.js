e17e569d482b3c9648ddd7b378eab92a4e8c1269
ozgurhanpolat <ozgurhan.45@gmail.com>
2023-07-27 17:37:27 +0300

    Api's are created

---
+++ davon_project_front_end/src/helper/api.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+
+
+const baseUrl = "http://localhost:8080/api/v1/user";
+
+export const getAllUsers = () => {
+    const users = 
+            axios.get(baseUrl + "/all-users")
+            .then((response) => {
+                return response
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    return users;
+
+};
+
+export const addNewUser = (newUser) => {
+    const response = 
+            axios.post(baseUrl + "/add-user" , newUser)
+            .then((response) => {
+                 return response})
+            .catch((error) => {
+                return error;
+            });
+
+    return response;
+
+}
+
+
+export const updateUser = (updatedUser) => {
+    const response = 
+                axios.post(baseUrl + "/update-user", updatedUser)
+                .then((response) =>{
+                    return response
+                })
+                .catch((error) => {
+                    return error;
+                });
+
+    return response;
+}
+
+
+export const deleteUser = (userId) => {
+    const response = 
+                axios.delete(baseUrl + "/delete-user/" + userId)
+                .then( (response) => {
+                    return response
+                })
+                .catch( (error) => {
+                    return error;
+                })
+    return response;
+}
